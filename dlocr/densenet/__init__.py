96050a61865441ade89d3055958af6c4a01be1d4
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-03-02 10:25:49 +0800

    Remove unused codes of DenseNet.

--- dlocr/densenet/__init__.py
+++ dlocr/densenet/__init__.py
@@ -1,20 +1,3 @@
-import os
 
-from dlocr.densenet.core import DenseNetOCR
-from dlocr.utils import load_config
-
-default_densenet_weight_path = os.path.join(os.getcwd(), os.path.dirname(__file__),
-                                            "../weights/weights-densent-init.hdf5")
-default_densenet_config_path = os.path.join(os.getcwd(), os.path.dirname(__file__), "../config/densent-default.json")
-default_dict_path = os.path.join(os.getcwd(), os.path.dirname(__file__), "../dictionary/char_std_5990.txt")
-
-__densenet = None
-
-
-def get_or_create(densenet_config_path=default_densenet_config_path,
-                  densenet_weight_path=default_densenet_weight_path):
-    global __densenet
-    if __densenet is None:
-        config = load_config(densenet_config_path)
-        __densenet = DenseNetOCR(**config, weight_path=densenet_weight_path)
-    return __densenet
+from dlocr.densenet.model import get_model
+from dlocr.densenet.densenet_predict import predict, predict_multi
