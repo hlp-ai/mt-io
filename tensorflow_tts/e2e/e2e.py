9221b16e87423b47fe1a55dc827834f572f25793
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-05-11 14:30:53 +0800

    Change text.

--- tensorflow_tts/e2e/e2e.py
+++ tensorflow_tts/e2e/e2e.py
@@ -9,7 +9,7 @@ txt2mel_conf_fn = r"D:\kidden\github\TensorFlowTTS\examples\tacotron2\conf\tacot
 print("Loading txt2mel config from", txt2mel_conf_fn)
 txt2mel_conf = AutoConfig.from_pretrained(txt2mel_conf_fn)
 
-model_fn = r"D:\dataset\baker\baker\tacotron2\checkpoints\model-12000.h5"
+model_fn = r"D:\dataset\baker\baker\tacotron2\checkpoints\model-28000.h5"
 print("Loading txt2mel model from", model_fn)
 txt2mel = TFAutoModel.from_pretrained(model_fn, txt2mel_conf)
 
@@ -25,7 +25,7 @@ mapper_fn = r"D:\kidden\github\TensorFlowTTS\tensorflow_tts\processor\pretrained
 print("Loading mapper from", mapper_fn)
 processor = AutoProcessor.from_pretrained(mapper_fn)
 
-text = "我是机器人。吴雨晨在学我说话。吴雨晨的姐姐叫吴雨桐。"
+text = "语音合成是自然语言处理中一个研究领域。"
 input_ids = processor.text_to_sequence(text, inference=True)
 
 print("Text to Mel...")
@@ -44,6 +44,6 @@ for i, post_mel_output in enumerate(post_mel_outputs):
 
 audio_after = mel2wav.inference(post_mel_outputs)[0, :, 0]
 
-wav_fn = './audio_after.wav'
+wav_fn = './audio_baker_28k_80k.wav'
 print("Saving wav file into", wav_fn)
 sf.write(wav_fn, audio_after, 24000, "PCM_16")
