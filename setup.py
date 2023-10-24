449c73384901db296faaadbe4c50d299428afe34
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-10-24 15:50:39 +0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="yimt-ocr",
+    version="0.01",
+    license="MIT",
+    author="LiuXiaofeng",
+    author_email="kiddenliu@sina.com",
+    url="https://github.com/hlp-ai",
+    project_urls={
+        "Source": "https://github.com/hlp-ai/mt-io",
+    },
+    keywords="OCR for MT",
+    python_requires=">=3.7",
+    packages=find_packages(),
+)
